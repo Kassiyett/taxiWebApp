92b806ec66c3ab1731722b9ce77fb840c4a08626
kassiyetadilbay <adilbay@student.ubc.ca>
2025-07-01 13:08:25 -0700

    changes working

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import Image from 'next/image';
 
 function Header() {
+  const headerMenu = [
+    {
+      id:1,
+      name:"Ride",
+      icon: "/logo.png"
+    },
+    {
+      id: 2, 
+      name: "Package",
+      icon: "/banner.png"
+    }
+  ]
+
   return (
-        <div>
-            Header
+        <div className='p-5 pb-3 pl-10 border-b-[4px]
+         border-gray-200'>
+            <Image src="/logo.png" alt="Logo" width={70} height={70} />
         </div>
-  );
+  )
 }
-export default Header;
+export default Header
