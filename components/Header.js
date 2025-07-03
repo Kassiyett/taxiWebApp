b71c9e82b4f004a38c9c5ffacee443cf095f5c55
kassiyetadilbay <adilbay@student.ubc.ca>
2025-07-03 13:18:06 -0700

    Header with Icons

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
+import { UserButton } from "@clerk/nextjs";
+
 
 function Header() {
   const headerMenu = [
@@ -16,10 +18,24 @@ function Header() {
   ]
 
   return (
-        <div className='p-5 pb-3 pl-10 border-b-[4px]
-         border-gray-200'>
-            <Image src="/logo.png" alt="Logo" width={70} height={70} />
+        <div className='p-4 pb-3 pl-10 border-b-[4px]
+         border-gray-200 flex items-center justify-between'>
+          <div className='flex gap-24 items-center'>
+            <Image src="/logo.png" alt="Logo" 
+            width={70} height={70} />
+            <div className = 'flex gap-6 items-center'>
+              {headerMenu.map((item) => (
+                <div className='flex gap-2'>
+                <Image src = {item.icon}
+                width = {17} height = {17}/>  
+                <h2 className='text-[14px] font-medium'>{item.name}</h2>
+                </div>
+                ))}
+            </div>
+            <div/>
         </div>
+        <UserButton/>
+      </div>
   )
 }
 export default Header
